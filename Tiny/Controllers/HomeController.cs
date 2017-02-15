5b7a790241a2f8c5c46af952b301508eb2916a5e
Bobby <kissinger645@gmail.com>
2017-02-14 20:56:22 -0500

    Index Layout

--- Tiny/Controllers/HomeController.cs
+++ Tiny/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tiny.Models;
 
 namespace Tiny.Controllers
 {
     public class HomeController : Controller
     {
+        
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            ViewBag.AllLinks = db.Links.Where(l => l.Public == true).OrderByDescending(link => link.Created);
             return View();
         }
 
